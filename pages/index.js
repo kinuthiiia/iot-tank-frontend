37231372db54bf5d48c0db7fa1dd37fccb92c24b
Stephen Kinuthia <skinuthia800@gmail.com>
2023-01-16 11:06:36 +0300

    First commit

---
+++ pages/index.js
@@ -0,0 +1,147 @@
+import { io } from "socket.io-client";
+import { color } from "d3-color";
+import { interpolateRgb } from "d3-interpolate";
+import { useState, useEffect } from "react";
+import LiquidFillGauge from "react-liquid-gauge";
+
+const socket = io("ws://localhost:8000");
+
+export default function Home() {
+  const [level, setLevel] = useState(0);
+  const [isConnected, setIsConnected] = useState();
+
+  let startColor = "#6495ed"; // cornflowerblue
+  let endColor = "#dc143c";
+
+  const interpolate = interpolateRgb(startColor, endColor);
+  const fillColor = interpolate(level / 100);
+
+  const gradientStops = [
+    {
+      key: "0%",
+      stopColor: color(fillColor).darker(0.5).toString(),
+      stopOpacity: 1,
+      offset: "0%",
+    },
+    {
+      key: "50%",
+      stopColor: fillColor,
+      stopOpacity: 0.75,
+      offset: "50%",
+    },
+    {
+      key: "100%",
+      stopColor: color(fillColor).brighter(0.5).toString(),
+      stopOpacity: 0.5,
+      offset: "100%",
+    },
+  ];
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("Connected!");
+      setIsConnected(true);
+    });
+
+    socket.on("disconnect", () => {
+      setIsConnected(false);
+    });
+
+    socket.on("level", ({ data }) => {
+      setLevel(() => data);
+    });
+
+    return () => {
+      socket.off("connect");
+      socket.off("disconnect");
+      socket.off("level");
+    };
+  }, []);
+
+  useEffect(() => {
+    setIsConnected(socket.connected);
+  }, [socket.connected]);
+
+  const sendPumpSignal = () => {
+    if (!socket.connected) {
+      alert("System offline");
+      return;
+    }
+    socket.emit("start-pump", true, (val) => {
+      console.log(val);
+    });
+    alert("Pump started");
+  };
+
+  const sendTapSignal = () => {
+    if (!socket.connected) {
+      alert("System offline");
+      return;
+    }
+    socket.emit("open-tap", true, (val) => {
+      console.log(val);
+    });
+    alert("Tap opened");
+  };
+
+  // client-side
+
+  return (
+    <div className="p-8 space-y-4">
+      <h1 className="text-[2rem] font-bold ">WaterTank Level</h1>
+      <p className="font-semibold text-gray-500">
+        System status{"  "}
+        {isConnected ? (
+          <span class="bg-green-100 text-green-900 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">
+            Online
+          </span>
+        ) : (
+          <span class="bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300">
+            Offline
+          </span>
+        )}
+      </p>
+
+      <LiquidFillGauge
+        width={300}
+        height={400}
+        value={level}
+        percent="%"
+        textSize={0.5}
+        riseAnimation
+        riseAnimationTime={1500}
+        innerRadius={0.98}
+        waveAnimation
+        waveFrequency={2}
+        waveAmplitude={1}
+        gradient
+        gradientStops={gradientStops}
+        circleStyle={{
+          fill: fillColor,
+        }}
+        waveStyle={{
+          fill: fillColor,
+        }}
+      />
+
+      <hr />
+      <h1 className="text-[1.2rem] font-bold tracking-tight ">Actions</h1>
+      <div className="space-y-2 pt-8">
+        <button
+          onClick={sendPumpSignal}
+          type="button"
+          class=" w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+        >
+          Pump In Water
+        </button>
+        <button
+          onClick={sendTapSignal}
+          type="button"
+          class="w-full text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+        >
+          Open Discharge Tap
+        </button>
+      </div>
+    </div>
+  );
+}
